bbc45466b4bcbf03863a15a2e341c3aee332419c
vjdato21 <vincejoshuadato@gmail.com>
2022-02-10 11:56:01 +0800

    [Keychron C1 Series] More agnostic keymap
    - Now use one default keymap for every variant.
    - Defines are now used to tailor for any differentiating keys across the variants.
    
    Thanks to @tbowmo and @dexter93 for this nice suggestion.
    
    Co-Authored-By: Thomas Mørch <5305197+tbowmo@users.noreply.github.com>
    Co-Authored-By: dexter93 <d3xter93@gmail.com>

--- keyboards/keychron/c1/keymaps/default/keymap.c
+++ keyboards/keychron/c1/keymaps/default/keymap.c
@@ -1,25 +1,138 @@
-/* Copyright 2020 Adam Honse <calcprogrammer1@gmail.com>
- * Copyright 2020 Dimitris Mantzouranis <d3xter93@gmail.com>
- * Copyright 2022 Harrison Chan (Xelus)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*
+Copyright 2020 Dimitris Mantzouranis
 
-#if defined(KEYBOARD_keychron_c1_rgb)
-    #include "rgb.c"
-#elif defined(KEYBOARD_keychron_c1_white)
-    #include "white.c"
-#elif defined(KEYBOARD_keychron_c1_plain)
-    #include "plain.c"
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include QMK_KEYBOARD_H
+
+/* Keychron Fn */
+
+#define KC_TASK LGUI(KC_TAB)        // Task viewer
+#define KC_FLXP LGUI(KC_E)          // Windows file explorer
+#define KC_CORT LGUI(KC_C)          // Cortana
+#define KC_SNIP LGUI(LSFT(KC_S))    // Windows snip tool
+#define KC_W_FN MO(3)               // Windows Fn
+
+#define KC_MSSN LGUI(KC_F3)         // Mission Control
+#define KC_FIND LALT(LGUI(KC_SPC))  // Finder
+#define KC_SIRI LGUI(KC_SPC)        // Siri
+#define KC_MSCR LSFT(LGUI(KC_3))    // Mac screenshot
+#define KC_MSNP LSFT(LGUI(KC_4))    // Mac snip tool
+#define KC_M_FN MO(1)               // Mac Fn
+
+/* Variant-tailored Defines */
+
+#if defined(LED_MATRIX_ENABLE)
+    #define LAYOUT_tkl_ansi LAYOUT_tkl_ansi_260
+    #define LGT_VAD BL_DEC              // Decrease KB Brightness
+    #define LGT_VAI BL_INC              // Increase KB Brightness
+    #define EXTRA_A KC_TRANS            // None
+    #define EXTRA_B BL_TOGG             // Toggle BL
+    #define EXTRA_C KC_TRANS            // None
+    #define EXTRA_D KC_TRANS            // None
+    #define EXTRA_E KC_TRANS            // None
+    #define EXTRA_F KC_TRANS            // None
+#elif defined(RGB_MATRIX_ENABLE)
+    #define LAYOUT_tkl_ansi LAYOUT_tkl_ansi_240B
+    #define LGT_VAD RGB_VAD             // Decrease KB Brightness
+    #define LGT_VAI RGB_VAI             // Increase KB Brightness
+    #define EXTRA_A RGB_TOG             // Toggle RGB
+    #define EXTRA_B RGB_MOD             // Cycle RGB Modes
+    #define EXTRA_C RGB_HUI             // Increase RGB Hue
+    #define EXTRA_D RGB_HUD             // Decrease RGB Hue
+    #define EXTRA_E RGB_SAI             // Increase RGB Saturation
+    #define EXTRA_F RGB_SAD             // Decrease RGB Saturation
+#else
+    #define LAYOUT_tkl_ansi LAYOUT_tkl_ansi_260
+    #define LGT_VAD KC_TRANS            // Decrease KB Brightness
+    #define LGT_VAI KC_TRANS            // Increase KB Brightness
+    #define EXTRA_A LCMD(LCTL(KC_Q))    // Mac Lock
+    #define EXTRA_B KC_LGUI(KC_L)       // Windows Lock
+    #define EXTRA_C KC_TRANS            // None
+    #define EXTRA_D KC_TRANS            // None
+    #define EXTRA_E KC_TRANS            // None
+    #define EXTRA_F KC_TRANS            // None
 #endif
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    /*  Mac layout
+    +-------------------------------------------------------------------------------------------+
+    | ESC |  | F1 | F2 | F3 | F4 | | F5 | F6 | F7 | F8 | | F9| F10| F11| F12|  | |PSCR|SIRI| BL |
+    +--------------------------------------------------------------------------+ +--------------+
+    |  ~  |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |  - |  = | BACKSP | |INS |HOME|PGUP|
+    +--------------------------------------------------------------------------+ +--------------+
+    |  TAB  |  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P |  [ |  ] |   \  | |DEL |END |PGDN|
+    +--------------------------------------------------------------------------+ +--------------+
+    | CAPSLCK  |  A |  S |  D |  F |  G |  H |  J |  K |  L | ; | ' |  RETURN  |                |
+    +--------------------------------------------------------------------------+      +----+    |
+    | LSHIFT     |  Z |  X |  C |  V |  B |  N |  M | , | . |  / |   RSHIFT    |      | UP |    |
+    +--------------------------------------------------------------------------+ +--------------+
+    |LCTRL| LALT| LGUI |            SPACE            | RGUI| RALT | FN | RCTRL | |LFT |DWN |RGT |
+    +-------------------------------------------------------------------------------------------+
+    */
+
+    [0] = LAYOUT_tkl_ansi( \
+        KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MSCR,  KC_SIRI,  EXTRA_A, \
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP, \
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN, \
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,                                \
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,             \
+        KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                                 KC_RGUI,  KC_RALT,  KC_M_FN,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT  \
+    ),
+
+    [1] = LAYOUT_tkl_ansi( \
+        RESET,              KC_BRID,  KC_BRIU,  KC_MSSN,  KC_FIND,  LGT_VAD,  LGT_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_MSNP,  _______,  EXTRA_B, \
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______, \
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,                               \
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            EXTRA_C,           \
+        _______,  _______,  GUI_TOG,                                _______,                                GUI_TOG,  _______,  _______,  _______,  EXTRA_F,  EXTRA_D,  EXTRA_E  \
+    ),
+
+    /*  Windows layout
+    +-------------------------------------------------------------------------------------------+
+    | ESC |  | F1 | F2 | F3 | F4 | | F5 | F6 | F7 | F8 | | F9| F10| F11| F12|  | |PSCR|CORT| BL |
+    +--------------------------------------------------------------------------+ +--------------+
+    |  ~  |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |  - |  = | BACKSP | |INS |HOME|PGUP|
+    +--------------------------------------------------------------------------+ +--------------+
+    |  TAB  |  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P |  [ |  ] |   \  | |DEL |END |PGDN|
+    +--------------------------------------------------------------------------+ +--------------+
+    | CAPSLCK  |  A |  S |  D |  F |  G |  H |  J |  K |  L | ; | ' |  RETURN  |                |
+    +--------------------------------------------------------------------------+      +----+    |
+    | LSHIFT     |  Z |  X |  C |  V |  B |  N |  M | , | . |  / |   RSHIFT    |      | UP |    |
+    +--------------------------------------------------------------------------+ +--------------+
+    |LCTRL| LGUI| LALT |            SPACE            | RALT| RGUI | FN | RCTRL | |LFT |DWN |RGT |
+    +-------------------------------------------------------------------------------------------+
+    */
+
+    [2] = LAYOUT_tkl_ansi( \
+        KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_CORT,  EXTRA_B, \
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP, \
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN, \
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,                                \
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,             \
+        KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RGUI,  KC_W_FN,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT  \
+    ),
+
+    [3] = LAYOUT_tkl_ansi( \
+        RESET,              KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  LGT_VAD,  LGT_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_SNIP,  _______,  EXTRA_A, \
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______, \
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,                               \
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            EXTRA_C,           \
+        _______,  GUI_TOG,  _______,                                _______,                                _______,  GUI_TOG,  _______,  _______,  EXTRA_F,  EXTRA_D,  EXTRA_E  \
+    )
+};
