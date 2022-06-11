83379730c33d783b7c0a194d5d544a0655daf3d1
vjdato21 <vjdato21@outlook.com>
2022-06-12 01:26:55 +0800

    [Keychron C1] Set debounce for all
    - Added debounce for all variants.

--- keyboards/keychron/c1/config.h
+++ keyboards/keychron/c1/config.h
@@ -31,3 +31,6 @@
 
 /* Polling Rate */
 #define USB_POLLING_INTERVAL_MS 1
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE            5
