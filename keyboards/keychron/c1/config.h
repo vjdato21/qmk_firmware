86e6f328c32ee4f6b725830139ee26f8303469d3
vjdato21 <vjdato21@outlook.com>
2022-11-15 18:54:14 +0800

    [Keychron C1] Reduce debounce
    - Playing around with debounce (trying to go with the sweetest spot).
    - Multiple commits involving debounce may appear in future commits. (sorry, I don't have the compiler with my laptop right now coz my SSD is in the brink of death)

--- keyboards/keychron/c1/config.h
+++ keyboards/keychron/c1/config.h
@@ -33,4 +33,4 @@
 #define USB_POLLING_INTERVAL_MS 1
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE            5
+#define DEBOUNCE            1
